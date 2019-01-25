f6f6bd4b0776e618374af615932f63e37e6c5030
Joaquin Fox <joaquin.fox@gmail.com>
2019-01-25 09:54:21 -0700

    basic server setup with dummy data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-
+const bodyParser = require('body-parser');
 const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
@@ -15,6 +15,7 @@ app.use(
     skip: (req, res) => process.env.NODE_ENV === 'test'
   })
 );
+app.use(bodyParser.json());
 
 app.use(
   cors({
@@ -22,6 +23,96 @@ app.use(
   })
 );
 
+let cats = [
+  {
+    imageURL:
+      'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription:
+      'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+  {
+    imageURL:
+      'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription:
+      'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Stuffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  }
+];
+
+let dogs = [
+  {
+    imageURL:
+      'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription:
+      'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL:
+      'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription:
+      'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Mr Dog',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  }
+];
+
+//Make mutable lists
+// let dogList = [...dogs];
+// let catList = [...cats];
+
+// *********ROUTES**************************
+//CAT******************
+// Retrieve a single cat at index 0
+app.get('/api/cat', (req, res) => {
+  let oldestCat = cats[0];
+  return res.json({
+    oldestCat
+  });
+});
+//
+app.delete('/api/cat', (req, res, next) => {
+  if (!cats.length) {
+    res.json({ message: 'There are no cats for adoption today' });
+  }
+
+  cats.shift();
+  res.status(204).end();
+});
+
+//DOG******************
+app.get('/api/dog', (req, res) => {
+  let oldestDog = dogs[0];
+  return res.json({
+    oldestDog
+  });
+});
+app.delete('/api/dog', (req, res, next) => {
+  if (!dogs.length) {
+    res.json({ message: 'There are no dogs for adoption today' });
+  }
+
+  dogs.shift();
+  res.status(204).end();
+});
+
+/********************************************* */
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
